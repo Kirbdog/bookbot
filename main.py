7cc21d033c8be1f7d0dfda32d796190f1c99f903
Kirbdog <kirbdog315@gmail.com>
2024-10-11 00:01:17 -0500

    added count_words function to main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def count_words(file):
+    file_as_array = file.split()
+    num_words = 0
+
+    for word in file_as_array:
+        num_words += 1
+    return num_words
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+
+    print(count_words(file_contents))
+    
+    # print(file_contents)
