a73f8102f63389157abe94b9729a810a8de733af
Kirbdog <kirbdog315@gmail.com>
2024-10-12 22:56:13 -0500

    added num_letters function

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+def num_letters(file):
+    char_count = {}
+    for char in file:
+        lowered = char.lower()
+        if lowered in char_count:
+            char_count[lowered] += 1
+        else:
+            char_count[lowered] = 1
+
+    return char_count
+
 def count_words(file):
     file_as_array = file.split()
-    
     return len(file_as_array)
+
 
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
-    print(count_words(file_contents))
-    
-    # print(file_contents)
+
