9ae82d8a518e10295dae2c50fc11a60390c4fd45
Kirby Conrad <kirbdog315@gmail.com>
2025-07-30 16:45:57 -0500

    Added and implemented sort_on function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_characters
+from stats import sort_dict
 
 def get_book_text(filepath):
     file_contents = None
@@ -9,10 +10,12 @@ def get_book_text(filepath):
 
 
 def main():
-    print(get_book_text('books/frankenstein.txt'))
     book_text = get_book_text('books/frankenstein.txt')
     print(f'{count_words(book_text)} words found in the document')
+
     num_chars = count_characters(book_text)
     print(f'The number of each character in the book is {num_chars}')
+
+    sorted_text_info = sort_dict(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,13 @@ def count_characters(book_text):
         else:
             character_dict[char] = 1
     return character_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(dictionary_to_sort):
+    char_count_list = []
+    for char in dictionary_to_sort:
+        char_count_list.append({"char": char, "num": dictionary_to_sort.get(char)})
+    char_count_list.sort(reverse=True, key=sort_on)
+
